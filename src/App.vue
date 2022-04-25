deb542f0bff535e8513c196b40757a998b091bbe
PMV-cute <maksL_00@mail.ru>
2022-04-25 21:52:19 +0300

    lab5

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+<win></win>
+</div>
+</template>
+
+<script lang="ts">
+import win from './components/win_f.vue'
+
+export default ({
+  name: 'App',
+  components: {
+    win
+  }
+});
+</script>
+
+<style>
+
+</style>
